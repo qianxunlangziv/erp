d7a206ec89a181f14268ba1abaddfd8e0ec79904
lcq <2575930659@qq.com>
2018-08-13 11:53:49 +0800

    test1

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ public class hello{
 	public static void main(){
 		System.out.println("hello");
 		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("1");
+		System.out.println("2");
 		Admin admin=new Admin();
 		System.out.println("1111111111111");
 		
