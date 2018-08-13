1e1d3493e753fea18156511b2f6416792678cfb1
lcq <2575930659@qq.com>
2018-08-13 15:23:22 +0800

    apply pacth

--- hello.java
+++ hello.java
@@ -4,6 +4,8 @@ public class hello{
 		System.out.println("hello");
 		System.out.println("1");
 		System.out.println("2");
+		System.out.println("hello");
+		System.out.println("hello");
 		Admin admin=new Admin();
 		System.out.println("1111111111111");
 		
