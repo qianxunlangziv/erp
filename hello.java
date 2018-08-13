2b4c21b398f65b20929e676d6be8f65079c90326
lcq <2575930659@qq.com>
2018-08-13 11:49:31 +0800

    test merge

--- hello.java
+++ hello.java
@@ -2,6 +2,8 @@ public class hello{
 	public static void main(){
 		System.out.println("hello");
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 		Admin admin=new Admin();
 		System.out.println("1111111111111");
 		
